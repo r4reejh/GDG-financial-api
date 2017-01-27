fce1fa34b27ef332c987033bcf25ddddc65a7b11
Reejh Ghosh <Reejh Ghosh>
2017-01-27 18:39:48 +0530

    Updated dependencies and routes

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post('/create',function(req,res){
 });
 
 router.get('/:sym',function(req,res){
-	Company.findOne({'company.symbol':req.params.sym},function(err,com){
+	Company.findOne({'company.symbol':(req.params.sym).toUpperCase()},function(err,com){
 		if(!err && com!=null){
 			res.send(com.company);
 			}
@@ -55,7 +55,7 @@ router.get('/:sym/stockprice/:exchange',function(req,res){
 		if(!err && com){
 			res.send(com.company.stock_price[req.params.exchange]);
 			}
-		else if(!com){
+		else if(com==null){
 			res.send("false");
 			}
 		else{
@@ -63,5 +63,64 @@ router.get('/:sym/stockprice/:exchange',function(req,res){
 		}
 	});
 });
+
+router.put('/:sym/update/all',function(req,res){
+	Company.findOne({'company.symbol':req.params.sym},function(err,com){
+		if(!err && com!=null){
+			com.company.ceo=req.body.ceo;
+			com.company.stock_price.nse=req.body.nse;
+			com.company.stock_price.bse=req.body.bse;
+			com.save(function(err,obj){
+				if(!err){
+					console.log("done");
+					res.send("true");
+				}
+			});
+			}
+		else if(com==null){
+			res.send("false");
+			}
+		else{
+			console.log(err);
+		}
+	});
+});
+
+router.put('/:sym/update/:field/:value',function(req,res){
+	console.log("HELLO");
+	Company.findOne({'company.symbol':req.params.sym},function(err,com){
+		if(!err && com!=null){
+			try {
+				console.log("HELLO");
+				if(com.company[req.params.field]!=undefined){
+				com.company[req.params.field]=req.params.value;
+				com.save(function(err,obj){
+					if(!err){
+						console.log("done");
+						res.send("true");
+					}
+					else{
+						res.send(err);
+					}
+				});
+				}
+				else{
+					res.send("incorrect command");
+					}
+			}
+			catch(err){
+				res.send(err);
+			}
+			
+		}
+		else if(com==null){
+			res.send("false");
+		}
+		else{
+			console.log(err);
+		}
+	});
+});
+
 
 module.exports = router;
